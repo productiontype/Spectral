f89366613f7deb4f2f92cd6e0e905311aab6a3cc
Emma Marichal <64773544+emmamarichal@users.noreply.github.com>
2024-10-17 12:35:39 +0200

    Update 2.005 (#22)
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * Update 2.005
    
    * URL link updated in OFL
    
    * froze requirements.txt
    
    * Update 2.005

--- sources/generate.sh
+++
@@ -1,4 +0,0 @@
-fontmake -m spectral-build-roman.designspace -i -o otf
-fontmake -m spectral-build-italic.designspace -i -o otf
-fontmake -m spectral-build-roman.designspace -i -o ttf
-fontmake -m spectral-build-italic.designspace -i -o ttf
